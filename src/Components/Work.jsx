6db4a464fcde85a9f1c2bdb563078abc1f1ce394
Jatin-Ahirwar <jatinahirwar24@gmail.com>
2024-03-21 23:30:20 +0530

    footer changes

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -6,12 +6,12 @@ const Work = () => {
   
   return (
     <div className='w-full overflow-x-hidden flex flex-col max-xs:px-0 max-phone:gap-0 px-8 gap-10'>
-        <h2 className='text-xl no-scrollbar max-xs:px-8'>Work</h2>
+        <h2 className='text-xl no-scrollbar max-xs:px-4'>Work</h2>
         <AllFrontend/>     
         <div className='  flex justify-center  max-xs:pb-10 max-xs:pt-4 '>
             <Link to="/work" 
               style={{ transition: 'background-color 0.3s ease-in-out, color 0.3s ease-in-out' }}
-              className=' uppercase border border-[black] py-4 px-7 flex items-center justify-center  text-sm font-black rounded-full hover:bg-[#1F2024] hover:text-[#FEFEFE]'>
+              className=' uppercase border border-[black] max-phone:py-3 max-phone:px-6 py-4 px-7 flex items-center justify-center text-sm font-black rounded-full hover:bg-[#1F2024] hover:text-[#FEFEFE]'>
                 all work
             </Link>
         </div>        
