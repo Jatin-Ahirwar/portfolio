a9f264d3089b15f5f8db2d928d32384f982ea5db
Jatin-Ahirwar <jatinahirwar24@gmail.com>
2024-03-22 00:40:22 +0530

    backend connect

--- utils/axios.jsx
+++ utils/axios.jsx
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const instance = axios.create({
     // baseURL:"http://localhost:3030/ || https://portfolio-backend-zwjr.onrender.com",
-    baseURL:"http://localhost:3030/",
+    // baseURL:"http://localhost:3030/",
+    baseURL:"https://portfolio-backend-p4bo.onrender.com",
     withCredentials:true
 })
 
